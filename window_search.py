5436f8e6a54c4300df0ba2f81a9650dc2ebe6b0b
regis <jerzy.wenta@protonmail.com>
2019-06-08 16:07:09 +0200

    searching by many categories at the same time added

--- window_search.py
+++ window_search.py
@@ -21,9 +21,34 @@ class WindowSearch(QWidget):
         self.search_textbox.setPlaceholderText("Enter searched name")
         h_layout.addWidget(self.search_textbox)
 
-        self.combo_box = QComboBox()
-        self.combo_box.addItems(self.df.columns)
-        h_layout.addWidget(self.combo_box)
+        self.search_min_age = QLineEdit(self)
+        self.search_min_age.setPlaceholderText("Min Age")
+        h_layout.addWidget(self.search_min_age)
+
+        self.search_max_age = QLineEdit(self)
+        self.search_max_age.setPlaceholderText("Max Age")
+        h_layout.addWidget(self.search_max_age)
+
+        self.overall_min = QLineEdit(self)
+        self.overall_min.setPlaceholderText("Minimal Overall")
+        h_layout.addWidget(self.overall_min)
+
+        self.overall_max = QLineEdit(self)
+        self.overall_max.setPlaceholderText("Maximal Overall")
+        h_layout.addWidget(self.overall_max)
+
+        self.skill_moves_min = QLineEdit(self)
+        self.skill_moves_min.setPlaceholderText("Minimall skill moves")
+        h_layout.addWidget(self.skill_moves_min)
+
+        self.skill_moves_max = QLineEdit(self)
+        self.skill_moves_max.setPlaceholderText("Max skill moves")
+        h_layout.addWidget(self.skill_moves_max)
+
+        self.combo_box_position = QComboBox()
+        positions = list(self.df['Position'].dropna().unique())
+        self.combo_box_position.addItems(positions)
+        h_layout.addWidget(self.combo_box_position)
 
         self.search_button = QPushButton("Search", self)
         h_layout.addWidget(self.search_button)
@@ -43,11 +68,29 @@ class WindowSearch(QWidget):
 
     def search_clicked(self):
         entered_text = self.search_textbox.text()
-        entered_category = self.combo_box.currentText()
-        if entered_category in ['Name', 'Nationality', 'Club', 'Position', 'Height', 'Weight']:
-            result = self.df[self.df[entered_category].str.contains(entered_text, na=False)]
-        else:
-            result = self.df[self.df[entered_category] == float(entered_text)]
+        print(entered_text)
+        entered_position = self.combo_box_position.currentText()
+        print(entered_position)
+        min_age = float(self.search_min_age.text()) if self.search_min_age.text() != '' else 10
+        print(min_age)
+        max_age = float(self.search_max_age.text()) if self.search_max_age.text() != '' else 50
+        print(max_age)
+        min_overall = float(self.overall_min.text()) if self.overall_min.text() != '' else 20
+        print(min_overall)
+        max_overall = float(self.overall_max.text()) if self.overall_max.text() != '' else 100
+        print(max_overall)
+        min_skill_moves = float(self.skill_moves_min.text()) if self.skill_moves_min.text() != '' else 1.0
+        print(min_skill_moves)
+        max_skill_moves = float(self.skill_moves_max.text()) if self.skill_moves_max.text() != '' else 5.0
+        print(max_skill_moves)
+
+        result = self.df[
+            (self.df['Name'].str.contains(entered_text, na=False)) &
+            (self.df['Position'] == entered_position) &
+            (self.df['Age'] <= max_age) & (self.df['Age'] >= min_age) &
+            (self.df['Skill Moves'] <= max_skill_moves) & (self.df['Skill Moves'] >= min_skill_moves) &
+            (self.df['Overall'] < max_overall) & (self.df['Overall'] > min_overall)
+        ]
 
         self.model.update(result)
 
@@ -57,3 +100,4 @@ class WindowSearch(QWidget):
                    'Wage', 'Skill Moves', 'Position', 'Height', 'Weight']
 
         return df[columns]
+
