5ac2c26516a3a65a681d807f88c463b4761b1e99
regis <jerzy.wenta@protonmail.com>
2019-06-08 12:26:49 +0200

    added searching by choosen category

--- window_search.py
+++ window_search.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 from PyQt5.QtWidgets import QWidget, QPushButton, QLineEdit, QTableWidget, QTableWidgetItem, QVBoxLayout, QHBoxLayout, \
-    QFileDialog
+    QFileDialog, QComboBox
 from PyQt5.QtWidgets import QApplication, QTableView
 
 from pandas_model import PandasModel
@@ -11,22 +11,28 @@ class WindowSearch(QWidget):
     def __init__(self, parent, df):
         QWidget.__init__(self, parent=None)
 
-        self.df = df
+        self.df = self.prepare_df(df)
 
-        v_layout = QVBoxLayout(self)
+        v_layout = QVBoxLayout()
         h_layout = QHBoxLayout()
 
         self.search_textbox = QLineEdit(self)
         self.search_textbox.setPlaceholderText("Enter searched name")
         h_layout.addWidget(self.search_textbox)
 
+        self.combo_box = QComboBox()
+        self.combo_box.addItems(self.df.columns)
+        h_layout.addWidget(self.combo_box)
+
         self.search_button = QPushButton("Search", self)
         h_layout.addWidget(self.search_button)
 
         v_layout.addLayout(h_layout)
-        self.table = QTableView(self)
 
+        self.table = QTableView(self)
         v_layout.addWidget(self.table)
+
+        self.setLayout(v_layout)
 
         self.model = PandasModel(self.df)
         self.table.setModel(self.model)
@@ -36,6 +42,12 @@ class WindowSearch(QWidget):
 
     def search_clicked(self):
         entered_text = self.search_textbox.text()
-        print(entered_text)
-        result = self.df.query('Name.str.contains("{0}")'.format(entered_text))
+        entered_category = self.combo_box.currentText()
+        print(self.combo_box.activated[str])
+        result = self.df[self.df[entered_category].str.match(entered_text)]
         self.model.update(result)
+
+    def prepare_df(self, df):
+        columns = ['Name', 'Age', 'Nationality', 'Overall', 'Club', 'Value',
+                   'Wage', 'Skill Moves', 'Position', 'Height', 'Weight']
+        return df[columns]
